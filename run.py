f31e254fc72eabde1c0c2e104072e396f78ffaf2
Harry Mumford <harry.mumford@aluma.io>
2023-01-20 18:17:48 +0000

    Add printer

--- run.py
+++ run.py
@@ -2,8 +2,16 @@ from enum import Enum
 from time import sleep
 import copy
 import os
+import random
 
-Direction = Enum('Direction', ['NE', 'E', 'SE', 'SW', 'W', 'NW'])
+
+class Direction(Enum):
+    NE = 0
+    E = 1
+    SE = 2
+    SW = 3
+    W = 4
+    NW = 5
 
 
 class Coord:
@@ -13,7 +21,6 @@ class Coord:
 
     def __str__(self) -> str:
         return '[{0}, {1}]'.format(self.x, self.y)
-
 
 class Position():
     SOUTH = [
@@ -35,33 +42,46 @@ class Movement():
         self.board = board
 
     def move(self, coord: Coord, direction: Direction) -> Coord: 
-        if self.board.get(coord) == 2:
-            if direction == Direction.E:
-                new_coord = Coord(coord.x+1, coord.y)
-            if direction == Direction.SE:
-                if coord.y % 2 == 0:
-                    new_coord = Coord(coord.x, coord.y+1)
-                else:
-                    new_coord = Coord(coord.x+1, coord.y+1)
-            if direction == Direction.SW:
-                if coord.y % 2 == 0:
-                    new_coord = Coord(coord.x-1, coord.y+1)
-                else:
-                    new_coord = Coord(coord.x, coord.y+1)
-            if direction == Direction.W:
-                new_coord = Coord(coord.x-1, coord.y)
-            if direction == Direction.NW:
-                if coord.y % 2 == 0:
-                    new_coord = Coord(coord.x-1, coord.y-1)
-                else:
-                    new_coord = Coord(coord.x, coord.y-1)
-            if direction == Direction.NE:
-                if coord.y % 2 == 0:
-                    new_coord = Coord(coord.x, coord.y-1)
-                else:
-                    new_coord = Coord(coord.x+1, coord.y-1)
+        adjacent_coord = self._get_adjacent_coord(coord, direction)
+        if self.board.get_state(adjacent_coord) == 0:
+            return None
         
-        self.board.move(coord, new_coord)
+        if self.board.get_state(adjacent_coord) == 1:
+            self.board.move(coord, adjacent_coord)
+            return adjacent_coord
+        
+        jump_dest = self._get_adjacent_coord(adjacent_coord, direction)
+        if self.board.get_state(jump_dest) == 0 or self.board.get_state(jump_dest) == 2:
+            return None
+        
+        self.board.move(coord, jump_dest)
+        return jump_dest
+        
+    def _get_adjacent_coord(self, coord: Coord, direction: Direction) -> Coord:
+        if direction == Direction.E:
+            new_coord = Coord(coord.x+1, coord.y)
+        if direction == Direction.SE:
+            if coord.y % 2 == 0:
+                new_coord = Coord(coord.x, coord.y+1)
+            else:
+                new_coord = Coord(coord.x+1, coord.y+1)
+        if direction == Direction.SW:
+            if coord.y % 2 == 0:
+                new_coord = Coord(coord.x-1, coord.y+1)
+            else:
+                new_coord = Coord(coord.x, coord.y+1)
+        if direction == Direction.W:
+            new_coord = Coord(coord.x-1, coord.y)
+        if direction == Direction.NW:
+            if coord.y % 2 == 0:
+                new_coord = Coord(coord.x-1, coord.y-1)
+            else:
+                new_coord = Coord(coord.x, coord.y-1)
+        if direction == Direction.NE:
+            if coord.y % 2 == 0:
+                new_coord = Coord(coord.x, coord.y-1)
+            else:
+                new_coord = Coord(coord.x+1, coord.y-1)
         return new_coord
              
 class Board():
@@ -71,25 +91,40 @@ class Board():
         self.history.append(copy.deepcopy(initial_state))
         
     def populate_all(self, coords):
-        current_state = self._get_current_state()
+        current_state = self._get_all_states()
         for coord in coords:
             current_state[coord.y][coord.x] = 2
         self._update_history(current_state)
         
     def move(self, old_coord, new_coord):
-        current_state = self._get_current_state()
+        current_state = self._get_all_states()
         current_state[old_coord.y][old_coord.x] = 1
         current_state[new_coord.y][new_coord.x] = 2
         self._update_history(current_state)
-                    
-    def get(self, coord: Coord) -> int:
-        current_state = self._get_current_state()
+        
+    def get_state(self, coord: Coord) -> int:
+        current_state = self._get_all_states()
+        if len(current_state)-1 < coord.y or len(current_state[0])-1 < coord.x:
+            return 0
         return current_state[coord.y][coord.x]
     
     def _update_history(self, board_state):
         self.history.append(board_state)
         
-    def _get_current_state(self):
+    def get_coords_with_state(self, state: int):
+        coords = []
+        y = 0
+        for row in self._get_all_states():
+            x = 0
+            for s in row:
+                if s == state:
+                    coords.append(Coord(x, y))
+                x += 1
+            y += 1
+               
+        return coords
+        
+    def _get_all_states(self):
         return copy.deepcopy(self.history[-1])
 
 
@@ -102,6 +137,7 @@ class Animation():
     frames = []
 
     def __init__(self, state_history) -> None:
+        self.state_history = state_history 
         for state in state_history:
             frame_str = ""
             count = 0
@@ -115,16 +151,29 @@ class Animation():
                         frame_str += "○   "
                     if pixel == 2:
                         frame_str += "●   "
+                if count % 2 == 0:
+                    frame_str += "  "
                 frame_str += "\n"
                 count += 1
             self.frames.append(frame_str)
             
+    def print(self) -> None:
+        str = ""
+        i = 0
+        while i <len(self.state_history[0]):
+            for f in self.frames:
+                lines = f.splitlines()
+                str += lines[i]
+            str += "\n"
+            i += 1
+            
+        print(str)
+            
     def run(self) -> None:
-        sleep(1)
         os.system('clear')
         for f in self.frames:
             print(f)
-            sleep(0.5)
+            sleep(1)
             os.system('clear')
 
 def run():
@@ -152,23 +201,29 @@ def run():
     board = Board(copy.deepcopy(initial_state))
     movement = Movement(board)
 
-    board.populate_all(p1.starting_coords)
-
+    # board.populate_all(p1.starting_coords)
+    # directions = []
+    # for d in Direction:
+    #     directions.append(d)
     
-    dest = movement.move(Coord(4, 13), Direction.NW)
-    dest = movement.move(dest, Direction.NW)
-    dest = movement.move(dest, Direction.NW)
-    dest = movement.move(dest, Direction.NE)
-    dest = movement.move(dest, Direction.NE)
-    dest = movement.move(dest, Direction.SE)
-    dest = movement.move(dest, Direction.SE)
-    dest = movement.move(dest, Direction.SW)
-    dest = movement.move(dest, Direction.SW)
-    dest = movement.move(dest, Direction.E)
-    dest = movement.move(dest, Direction.W)
-    
-
+    # while len(board.history) <= 20:
+    #     coords = board.get_coords_with_state(2)
+    #     piece_coord = random.choice(coords)
+    #     piece_new_coord = movement.move(piece_coord, random.choice(directions))
+    #     if jumped(piece_coord, piece_new_coord):
+    #         movement.move(piece_coord, random.choice(directions))
+            
+    board.populate_all([Coord(6, 8), Coord(6, 7), Coord(7, 5)])
+    dest = movement.move(Coord(6, 8), Direction.SW)
+    if jumped:
+        movement.move(dest, Direction.NE)
+        
     animator = Animation(board.history)
-    animator.run()
+    animator.print()
+    # animator.run()
     
+def jumped(coord: Coord, new_coord: Coord) -> bool:
+   if abs(coord.x - coord.x) == 2 or abs(coord.y - coord.y) == 2:
+       return True
+        
 run()
